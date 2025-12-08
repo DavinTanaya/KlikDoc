b7f3c8719298bbe7ffb8f070fb5661a7a4caec45
DavinTanaya <davin.tanaya@gmail.com>
2025-12-08 22:04:03 +0700

    fix: call

--- resources/views/dokter/chat/index.blade.php
+++ resources/views/dokter/chat/index.blade.php
@@ -538,15 +538,12 @@
     }
 
     const rtcConfig = {
+      iceTransportPolicy: "relay",
       iceServers: [{
-          urls: 'stun:stun.l.google.com:19302'
-        },
-        {
-          urls: 'turn:openrelay.metered.ca:443',
-          username: 'openrelayproject',
-          credential: 'openrelayproject'
-        }
-      ]
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject"
+      }]
     };
 
 
--- resources/views/layanan/chat_dokter/index.blade.php
+++ resources/views/layanan/chat_dokter/index.blade.php
@@ -498,17 +498,13 @@
     }
 
     const rtcConfig = {
+      iceTransportPolicy: "relay",
       iceServers: [{
-          urls: 'stun:stun.l.google.com:19302'
-        },
-        {
-          urls: 'turn:openrelay.metered.ca:443',
-          username: 'openrelayproject',
-          credential: 'openrelayproject'
-        }
-      ]
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject"
+      }]
     };
-
 
     let localStream = null;
     let pcs = {};
